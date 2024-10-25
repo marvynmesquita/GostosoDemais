d3d2774a5c00bb7aa5af5e2a40011fac5d6eb226
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2024-10-25 20:05:57 -0300

    Criado models

---
+++ main/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+class Categorias(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+    
+
+class Ingredientes(models.Model):
+    nome = models.CharField(max_length=100)
+    quantidade = models.CharField(max_length=50)
+    receita = models.ForeignKey(Receitas, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+class Receitas(models.Model):
+    nome = models.CharField(max_length=100)
+    modo_preparo = models.TextField()
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='receitas/')
+    publicada = models.BooleanField(default=False)
+    ingredientes = models.ManyToManyField(Ingredientes)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tags')
+    criador = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+class Usuarios(models.Model):
+    usuario = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='usuarios/')
+    receitas = models.ManyToManyField(Receitas)
+    favoritas = models.ManyToManyField(Receitas, related_name='favoritas')
+    seguidos = models.ManyToManyField('self', related_name='seguidores', symmetrical=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.usuario.username
+    
+class Tags(models.Model):
+    nome = models.CharField(max_length=50)
+    receitas = models.ManyToManyField(Receitas)
+
+    def __str__(self):
+        return self.nome
+    
---
+++ main/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Categorias, Ingredientes, Receitas, Usuarios, Tags
+
+def index(request):
+    return HttpResponse('Hello, World!')
+
+def receitas(request):
+    receitas = Receitas.objects.all()
+    return render(request,'receitas.html', {'receitas': receitas})
+
+def receita_detail(request, receita_id):
+    receita = Receitas.objects.get(pk=receita_id)
+    return render(request,'receita_detail.html', {'receita': receita})
+
+def categorias(request):
+    categorias = Categorias.objects.all()
+    return render(request,'categorias.html', {'categorias': categorias})
+
+def categoria_detail(request, categoria_id):
+    categoria = Categorias.objects.get(pk=categoria_id)
+    return render(request,'categoria_detail.html', {'categoria': categoria})
+
+def ingredientes(request):
+    ingredientes = Ingredientes.objects.all()
+    return render(request,'ingredientes.html', {'ingredientes': ingredientes})
+
+def ingrediente_detail(request, ingrediente_id):
+    ingrediente = Ingredientes.objects.get(pk=ingrediente_id)
+    return render(request,'ingrediente_detail.html', {'ingrediente': ingrediente})
+
+
+def usuarios(request):
+    usuarios = Usuarios.objects.all()
+    return render(request,'usuarios.html', {'usuarios': usuarios})
+
+def usuario_detail(request, usuario_id):
+    usuario = Usuarios.objects.get(pk=usuario_id)
+    return render(request,'usuario_detail.html', {'usuario': usuario})
+
+def tags(request):
+    tags = Tags.objects.all()
+    return render(request,'tags.html', {'tags': tags})
+
+def tag_detail(request, tag_id):
+    tag = Tags.objects.get(pk=tag_id)
+    return render(request,'tag_detail.html', {'tag': tag})
+
+def buscar(request):
+    query = request.GET.get('query')
+    receitas = Receitas.objects.filter(nome__icontains=query)
+    return render(request,'buscar.html', {'receitas': receitas})
+
+
+# Create your views here.
