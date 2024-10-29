dda0bdd8941213adb17788c957c76c643ae1e12a
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2024-10-29 19:32:28 -0300

    chore: Automated Black

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Categorias(models.Model):
     nome = models.CharField(max_length=100)
@@ -7,7 +8,7 @@ class Categorias(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
 
 class Ingredientes(models.Model):
     nome = models.CharField(max_length=100)
@@ -16,38 +17,42 @@ class Ingredientes(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+
 class Receitas(models.Model):
     nome = models.CharField(max_length=100)
     modo_preparo = models.TextField()
     categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to='receitas/')
+    imagem = models.ImageField(upload_to="receitas/")
     publicada = models.BooleanField(default=False)
     ingredientes = models.ManyToManyField(Ingredientes)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField('Tags')
-    criador = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    tags = models.ManyToManyField("Tags")
+    criador = models.ForeignKey("auth.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
-    
+
+
 class Usuarios(models.Model):
-    usuario = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    foto = models.ImageField(upload_to='usuarios/')
+    usuario = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to="usuarios/")
     receitas = models.ManyToManyField(Receitas)
-    favoritas = models.ManyToManyField(Receitas, related_name='favoritas')
-    seguidos = models.ManyToManyField('self', related_name='seguidores', symmetrical=False)
+    favoritas = models.ManyToManyField(Receitas, related_name="favoritas")
+    seguidos = models.ManyToManyField(
+        "self", related_name="seguidores", symmetrical=False
+    )
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.usuario.username
-    
+
+
 class Tags(models.Model):
     nome = models.CharField(max_length=50)
     receitas = models.ManyToManyField(Receitas)
 
     def __str__(self):
         return self.nome
-    
